fec914d472e7b0a145652b3c3cb90e4bc9d58ce9
Joshua L <langley.joshua@gmail.com>
2019-02-06 00:14:08 +1100

    Re-organising old mu files in musqlserver

--- src/musqlserver/custTypedef.h
+++ src/musqlserver/custTypedef.h
@@ -1,46 +1,19 @@
+#pragma once
 
-#ifndef __CUSTTYPEDEF_H
-#define __CUSTTYPEDEF_H
-
-#ifndef USER_H__
-#define USER_H__
-#include "user.h"
-#endif
-
-typedef int BOOL;
+typedef bool BOOL;
 typedef wchar_t WCHAR;
+//typedef void VOID;
 
-typedef unsigned char UCHAR;
-typedef unsigned char* LPBYTE;
 typedef unsigned char BYTE;
-typedef unsigned short USHORT;
+typedef unsigned char* LPBYTE;
+
 typedef unsigned short WORD;
-typedef const WCHAR* LPCWSTR;
 typedef unsigned long DWORD;
+
+typedef const WCHAR CWSTR;
+typedef const WCHAR* LPCWSTR;
+
 typedef void* HANDLE;
 typedef unsigned long long UINT64;
-typedef char* LPSTR;
 
-typedef long long LONGLONG;
-typedef long long time_t;
-
-/*
-#ifndef _SYSTEMTIME
-#define _SYSTEMTIME
-typedef struct _SYSTEMTIME {
-	WORD wYear;
-	WORD wMonth;
-	WORD wDayOfWeek;
-	WORD wDay;
-	WORD wHour;
-	WORD wMinute;
-	WORD wSecond;
-	WORD wMilliseconds;
-} SYSTEMTIME, *PSYSTEMTIME, *LPSYSTEMTIME;
-typedef SYSTEMTIME* LPSYSTEMTIME;
-#endif
-*/
-
-typedef OBJECTSTRUCT* LPOBJECTSTRUCT;
-
-#endif
+typedef const char* LPSTR;
