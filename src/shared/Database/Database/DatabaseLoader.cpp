0073a87d77b3b1316e6e01ebdd5045e672d62ed5
Joshua L <langley.joshua@gmail.com>
2019-02-09 04:54:43 +1100

    DatabaseLoader Macros Removed 2

--- src/shared/Database/Database/DatabaseLoader.cpp
+++ src/shared/Database/Database/DatabaseLoader.cpp
@@ -25,88 +25,13 @@ DatabaseLoader& DatabaseLoader::AddDatabase(DatabaseWorkerPool<T>& pool, std::st
 {
     bool const updatesEnabledForThis = DBUpdater<T>::IsEnabled(_updateFlags);
 
-    _open.push([this, name, updatesEnabledForThis, &pool]() -> bool
-    {
-        uint8 const asyncThreads = 1;
-        uint8 const synchThreads = 1);
-
-        pool.SetConnectionInfo(_connConfig, asyncThreads, synchThreads);
-        if (uint32 error = pool.Open())
-        {
-            // Database does not exist
-            if ((error == ER_BAD_DB_ERROR) && updatesEnabledForThis && _autoSetup)
-            {
-                // Try to create the database and connect again if auto setup is enabled
-                if (DBUpdater<T>::Create(pool) && (!pool.Open()))
-                    error = 0;
-            }
-
-            // If the error wasn't handled quit
-            if (error)
-            {
-                sLog.outError("DatabasePool %s NOT opened. There were errors opening the MySQL connections.", name.c_str());
-
-                return false;
-            }
-        }
-        // Add the close operation
-        _close.push([&pool]
-        {
-            pool.Close();
-        });
-        return true;
-    });
-
-    // Populate and update only if updates are enabled for this pool
-    /*if (updatesEnabledForThis)
-    {
-        _populate.push([this, name, &pool]() -> bool
-        {
-            if (!DBUpdater<T>::Populate(pool))
-            {
-                sLog->outMessage(_logger, "Could not populate the %s database, see log for details.", name.c_str());
-                return false;
-            }
-            return true;
-        });
-
-        _update.push([this, name, &pool]() -> bool
-        {
-            if (!DBUpdater<T>::Update(pool))
-            {
-                sLog->outMessage(_logger, "Could not update the %s database, see log for details.", name.c_str());
-                return false;
-            }
-            return true;
-        });
-    }*/
-
-    _prepare.push([this, name, &pool]() -> bool
-    {
-        if (!pool.PrepareStatements())
-        {
-            sLog.outError("Could not prepare statements of the %s database, see log for details.", name.c_str());
-            return false;
-        }
-        return true;
-    });
-
     return *this;
 }
 
 bool DatabaseLoader::Load()
 {
-    //if (!_updateFlags)
-    //    MUSQL_LOG_INFO("sql.updates", "Automatic database updates are disabled for all databases!");
-
     if (!OpenDatabases())
         return false;
-
-    //if (!PopulateDatabases())
-    //    return false;
-
-    //if (!UpdateDatabases())
-    //    return false;
 
     if (!PrepareStatements())
         return false;
@@ -154,7 +79,3 @@ bool DatabaseLoader::Process(std::queue<Predicate>& queue)
     }
     return true;
 }
-
-
-//template DatabaseLoader& DatabaseLoader::AddDatabase<CharacterDatabaseConnection>(DatabaseWorkerPool<CharacterDatabaseConnection>&, std::string const&);
-//template DatabaseLoader& DatabaseLoader::AddDatabase<WorldDatabaseConnection>(DatabaseWorkerPool<WorldDatabaseConnection>&, std::string const&);
